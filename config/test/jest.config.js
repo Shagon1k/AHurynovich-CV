26acf61700151cedfe4aebb5c2cc10ca50566d3b
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2022-08-29 18:45:32 +0300

    [AH-42] Added Codecov report to CI/CD

--- config/test/jest.config.js
+++ config/test/jest.config.js
@@ -24,13 +24,13 @@ module.exports = {
     // clearMocks: false,
 
     // Indicates whether the coverage information should be collected while executing the test
-    collectCoverage: false, //IS_TEST_VERBOSE,
+    collectCoverage: IS_TEST_VERBOSE,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
     // collectCoverageFrom: undefined,
 
     // The directory where Jest should output its coverage files
-    coverageDirectory: 'coverage',
+    coverageDirectory: './test-reports/test-coverage',
 
     // An array of regexp pattern strings used to skip coverage collection
     coveragePathIgnorePatterns: ['/node_modules/'],
