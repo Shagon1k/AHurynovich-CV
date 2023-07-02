2ade823be6b406e2123c4c4bc8fc1fa752407d10
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2023-07-02 22:15:14 +0300

    [AH-M] Performance Enhance: Update lhci version; fix CLS errors

--- src/components/pages/ExperiencePage/components/PastProjects/PastProjects.container.tsx
+++ src/components/pages/ExperiencePage/components/PastProjects/PastProjects.container.tsx
@@ -5,19 +5,19 @@ const TO_API_pastProjectsData = [
         title: 'Burberry',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta vitae ad dignissimos consectetur expedita nihil saepe aperiam illo vero quisquam dolorum ipsa eius officia commodi nemo dolore iste earum, error totam laborum voluptates temporibus! Porro voluptas facere dignissimos explicabo blanditiis cumque numquam eveniet accusantium alias commodi quia, asperiores vitae aperiam.',
-        imgUrl: 'https://upcdn.io/W142hJk/raw/demo/4mLwbXrVFB.svg', // TODO: Move to AWS S3
+        imgUrl: 'https://svgshare.com/i/upv.svg', // TODO: Move to AWS S3
     },
     {
         title: 'Burberry. Redesign',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta vitae ad dignissimos consectetur expedita nihil saepe aperiam illo vero quisquam dolorum ipsa eius officia commodi nemo dolore iste earum, error totam laborum voluptates temporibus! Porro voluptas facere dignissimos explicabo blanditiis cumque numquam eveniet accusantium alias commodi quia, asperiores vitae aperiam.',
-        imgUrl: 'https://upcdn.io/W142hJk/raw/demo/4mLwbfJwFV.svg.svg', // TODO: Move to AWS S3
+        imgUrl: 'https://svgshare.com/i/ust.svg', // TODO: Move to AWS S3
     },
     {
         title: 'UKG',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta vitae ad dignissimos consectetur expedita nihil saepe aperiam illo vero quisquam dolorum ipsa eius officia commodi nemo dolore iste earum, error totam laborum voluptates temporibus! Porro voluptas facere dignissimos explicabo blanditiis cumque numquam eveniet accusantium alias commodi quia, asperiores vitae aperiam.',
-        imgUrl: 'https://upcdn.io/W142hJk/raw/demo/4mLwancvG3.svg', // TODO: Move to AWS S3
+        imgUrl: 'https://svgshare.com/i/usP.svg', // TODO: Move to AWS S3
     },
 ];
 
