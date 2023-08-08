616a7511fd41302d76a73d07cad5df57f06bd9e3
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2023-08-08 22:16:34 +0300

    [AH-R] Add small optimizations for SVGR

--- config/webpack/helpers/loaders/svg-react.loader.js
+++ config/webpack/helpers/loaders/svg-react.loader.js
@@ -2,5 +2,8 @@ export default () => ({
     loader: '@svgr/webpack',
     options: {
         titleProp: true, // Add ability to provide SVG title through property pass
+        descProp: true, // Add ability to provide SVG description through property pass
+        svgo: true, // SVG image optimization
+        memo: true, // Wrap all SVG's with React.memo (additional optimization)
     },
 });
