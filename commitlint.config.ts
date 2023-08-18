7541483f38fd01055afe2c4e121400885d770965
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2023-08-18 12:22:32 +0300

    [AH-E] Enhance TSConfig; Add commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,28 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^\[AH-(.+)\] (.+)/,
+            headerCorrespondence: ['scope', 'subject'],
+        },
+    },
+    plugins: [
+        {
+            rules: {
+                'header-match-pattern': (parsed) => {
+                    const { scope, subject } = parsed;
+                    if (scope === null && subject === null) {
+                        return [false, "Commit header must be in format: '[AH-X] Commit message'"];
+                    }
+                    return [true, ''];
+                },
+            },
+        },
+    ],
+    rules: {
+        'header-match-pattern': [2, 'always'],
+    },
+};
+
+module.exports = Configuration;
