5a2e581e5e65972f707b0d2ba1ac91d10dc07271
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2023-08-27 13:35:26 +0300

    [AH-7] Add Achievements block to specific skill if config contain it

--- src/components/pages/ExperiencePage/components/MyExpertise/MyExpertise.component.tsx
+++ src/components/pages/ExperiencePage/components/MyExpertise/MyExpertise.component.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { useSelector } from 'react-redux';
 
 import Icon, { type IIconName } from '@components/base/Icon';
+import Link from '@components/base/Link';
 import Section from '@components/base/Section';
 import { BP } from '@config/application';
 import { useTranslates } from '@reusables/custom-hooks';
@@ -12,7 +13,13 @@ import batteryLevelImgUrl from './images/battery-level.svg?url';
 
 import styles from './MyExpertise.module.scss';
 
-type IExperiencesList = { name: string; level: number; description: string; iconName: IIconName }[];
+type IExperiencesList = {
+    name: string;
+    level: number;
+    description: string;
+    iconName: IIconName;
+    achievements: { title: string; url: string; badgeUrl: string }[];
+}[];
 
 interface IMyExpertiseProps {
     myExperienceData: IExperiencesList;
@@ -44,42 +51,75 @@ const MyExpertise: React.FC<IMyExpertiseProps> = ({ myExperienceData }) => {
 
 type IMyExpertiseItemProps = IExperiencesList[number];
 
-const MyExpertiseItem: React.FC<IMyExpertiseItemProps> = ({ name, description, level, iconName }) => {
+const MyExpertiseItem: React.FC<IMyExpertiseItemProps> = ({
+    name,
+    description,
+    level,
+    iconName,
+    achievements,
+}) => {
     const { t } = useTranslates();
     const breakpointName = useSelector(selectAppBreakpoint);
     const iconSize = [BP.XS, BP.S].includes(breakpointName) ? 'm' : 'l';
+
+    const hasAchievements = achievements.length > 0;
 
     const levelDescription = t(
         `pages.experience.myExpertiseSection.levelToDescription.${Math.min(level, MAX_LEVEL)}`
     );
     return (
         <li className={styles['experiences-item']}>
-            <div className={styles['experience-tv-wrapper']}>
-                <h2 className={styles['experience-title']}>{name}</h2>
-                <div className={styles['experience-tv']}>
-                    <div className={styles['experience-tv-skill-icon']}>
-                        <Icon name={iconName} size={iconSize} />
+            <div className={styles['experience-main']}>
+                <div className={styles['experience-tv-wrapper']}>
+                    <h2 className={styles['experience-title']}>{name}</h2>
+                    <div className={styles['experience-tv']}>
+                        <div className={styles['experience-tv-skill-icon']}>
+                            <Icon name={iconName} size={iconSize} />
+                        </div>
+                        <div className={styles['experience-tv-battery']}>
+                            {Array.from({ length: level }, (_, i) => (
+                                // Having index as a key is "ok" for this case as equal elements rendered and order will not change
+                                <img
+                                    key={i}
+                                    className={styles['experience-tv-battery-level']}
+                                    src={batteryLevelImgUrl}
+                                    alt=''
+                                    aria-hidden={true}
+                                />
+                            ))}
+                        </div>
                     </div>
-                    <div className={styles['experience-tv-battery']}>
-                        {Array.from({ length: level }, (_, i) => (
-                            // Having index as a key is "ok" for this case as equal elements rendered and order will not change
-                            <img
-                                key={i}
-                                className={styles['experience-tv-battery-level']}
-                                src={batteryLevelImgUrl}
-                                alt=''
-                                aria-hidden={true}
-                            />
-                        ))}
-                    </div>
+                    <p className='visuallyhidden'>
+                        {t('pages.experience.myExpertiseSection.expertiseLevelDescription', {
+                            levelDescription,
+                        })}
+                    </p>
                 </div>
-                <p className='visuallyhidden'>
-                    {t('pages.experience.myExpertiseSection.expertiseLevelDescription', {
-                        levelDescription,
-                    })}
-                </p>
+                <span className={styles['experience-description']}>{description}</span>
             </div>
-            <span className={styles['experience-description']}>{description}</span>
+            {hasAchievements && (
+                <ul className={styles['experience-achievements']}>
+                    <span className='visuallyhidden'>
+                        {t('pages.experience.myExpertiseSection.achievementsListDescription')}
+                    </span>
+                    {achievements.map(({ title, url, badgeUrl }) => (
+                        <li key={title}>
+                            <Link
+                                className={styles['achievement-badge-link']}
+                                type='external'
+                                title={title}
+                                to={url}
+                            >
+                                <img
+                                    className={styles['achievement-badge-img']}
+                                    src={badgeUrl}
+                                    alt={`"${title}" ${t('common.badge')}`}
+                                />
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </li>
     );
 };
